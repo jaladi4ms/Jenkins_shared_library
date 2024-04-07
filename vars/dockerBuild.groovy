cc3ce7290a087e4147f012283d031ad00a3a4f2a
jaladi4ms <jaladi4ms@gmail.com>
2024-04-06 22:40:41 -0500

    docker images

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,10 @@
+def call(String dockerHubUsername, String imageName) {
+    // Build the Docker image
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=8ba2a8cb70msh66646143fb03b9cp1092a1jsn27926c8c79bb -t ${imageName} ."
+     // Tag the Docker image
+    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
+    // Push the Docker image
+    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
+        sh "docker push ${dockerHubUsername}/${imageName}:latest"
+    }
+}
